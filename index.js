08dc19a0765acf29c95cb0ac9c9b1fc596abfac0
Michael J Cerce <137245672+MichaelJCerce@users.noreply.github.com>
2023-07-24 16:09:54 -0400

    Add game loop to play x rounds

--- index.js
+++ index.js
@@ -1,34 +1,57 @@
-const actions = ["rock", "paper", "scissors"];
+const actions = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
   return actions[Math.floor(Math.random() * 3)];
 }
 
 function playRound(playerChoice, computerChoice) {
-  playerChoice = playerChoice.toLowerCase();
-  let result = "Draw!";
+  playerChoice = playerChoice.toUpperCase()[0] + playerChoice.slice(1);
+  let win;
+
   if (playerChoice === computerChoice) {
-    return result;
-  }
-  switch (playerChoice) {
-    case "rock":
-      computerChoice === "paper"
-        ? (result = "You Lose! Paper beats Rock!")
-        : (result = "You Win! Rock beats Scissors!");
-      break;
-    case "paper":
-      computerChoice === "scissors"
-        ? (result = "You Lose! Scissors beats Paper!")
-        : (result = "You Win! Paper beats Rock!");
-      break;
-    case "scissors":
-      computerChoice === "rock"
-        ? (result = "You Lose! Rock beats Scissors!")
-        : (result = "You Win! Scissors beats Paper!");
-      break;
-    default:
-      result = "That's cheating! You can only use Rock, Paper, or Scissors!";
+    console.log("Draw!");
+    return 0;
   }
 
-  return result;
+  switch (playerChoice) {
+    case "Rock":
+      computerChoice === "Paper" ? (win = false) : (win = true);
+      break;
+    case "Paper":
+      computerChoice === "Scissors" ? (win = false) : (win = true);
+      break;
+    case "Scissors":
+      computerChoice === "Rock" ? (win = false) : (win = true);
+      break;
+    default:
+      console.log("No cheating! You can only use Rock, Paper, or Scissors!");
+      return 0;
+  }
+  
+  if (win) {
+    console.log(`You Win! ${playerChoice} beats ${computerChoice}`);
+    return 1;
+  } else {
+    console.log(`You Lose! ${computerChoice} beats ${playerChoice}`);
+    return -1;
+  }
 }
+
+function game(rounds) {
+  let score = 0;
+  for (let i = 0; i < rounds; ++i) {
+    let playerChoice = prompt("Rock, Paper, or Scissors?");
+    let computerChoice = getComputerChoice();
+    score += playRound(playerChoice, computerChoice);
+  }
+
+  if (score > 0) {
+    console.log("You won!");
+  } else if (score === 0) {
+    console.log("You tied!");
+  } else {
+    console.log("You lost!");
+  }
+}
+
+game(5);
