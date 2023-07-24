34f2655d2768d53a7d8993818555986c9e9735f1
Michael J Cerce <137245672+MichaelJCerce@users.noreply.github.com>
2023-07-24 15:41:34 -0400

    Add logic to play one round

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
-const actions = ["Rock", "Paper", "Scissors"];
+const actions = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
-    return actions[Math.floor(Math.random() * 3)]
+  return actions[Math.floor(Math.random() * 3)];
+}
+
+function playRound(playerChoice, computerChoice) {
+  playerChoice = playerChoice.toLowerCase();
+  let result = "Draw!";
+  if (playerChoice === computerChoice) {
+    return result;
+  }
+  switch (playerChoice) {
+    case "rock":
+      computerChoice === "paper"
+        ? (result = "You Lose! Paper beats Rock!")
+        : (result = "You Win! Rock beats Scissors!");
+      break;
+    case "paper":
+      computerChoice === "scissors"
+        ? (result = "You Lose! Scissors beats Paper!")
+        : (result = "You Win! Paper beats Rock!");
+      break;
+    case "scissors":
+      computerChoice === "rock"
+        ? (result = "You Lose! Rock beats Scissors!")
+        : (result = "You Win! Scissors beats Paper!");
+      break;
+    default:
+      result = "That's cheating! You can only use Rock, Paper, or Scissors!";
+  }
+
+  return result;
 }
